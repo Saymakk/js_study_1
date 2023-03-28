163ff66ce114f069b1f4e35137557f2faf4a4f96
Vladislav <vladsarana@gmail.com>
2023-03-28 16:31:57 +0600

    initial

---
+++ js/script.js
@@ -0,0 +1,35 @@
+// /* eslint-disable no-unused-vars */
+// 'use strict';
+
+// let a = 15;
+// console.log(a);
+
+// console.log(1);
+// alert(1);
+
+// const obj = {
+// 	a: 1, 
+// 	b: 2,
+// 	c: 3,
+// 	d: {
+// 		a: 1, 
+// 		b: 3,
+// 		c: {a: 'd'},
+// 	},
+// };
+
+// console.log(obj);
+
+// obj.a = 1111;
+// console.log(obj);
+
+// obj.d = 1111;
+// console.log(obj);
+
+// const arr = [1, 2, 4,];
+
+// arr[10] = 'aaa';
+// console.log(arr[10]);
+// console.log(typeof arr);
+
+// document.write();
---
+++ js/typeOf_document.write.js
@@ -0,0 +1,38 @@
+/* eslint-disable no-unused-vars */
+'use strict';
+
+
+const isChecked = true;
+const isClose = true;
+console.log(isChecked || isClose);
+
+let incr = 10,
+	decr = 10;
+
+// ++incr;
+// --decr;
+
+console.log(++incr);
+console.log(--decr);
+
+console.log(5 == '5');
+
+
+// const result = confirm('Вы здесь?');
+
+// const answer = prompt('18?', 'Да');
+// console.log(typeof (answer));
+
+// const answer2 = +prompt('18?', 11);
+// console.log(typeof (answer2));
+
+// const arr = [];
+// arr[0] = prompt('Как ваше имя?', '');
+// arr[1] = prompt('Как ваша фамилия?', '');
+// arr[2] = prompt('Возраст', '');
+
+// document.write(arr);
+
+// const user = 'vlad';
+
+// alert(`Привет ${user}`);
